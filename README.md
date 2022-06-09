5f49ed5e33fef3406872126f9720a21c273f8c4f
icesix233 <11140788+icesix233@user.noreply.gitee.com>
2022-06-09 18:58:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+图书管理系统的后端
+nodejs+express+mysql
+数据库实训项目
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "booksys"
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/api/book/get", (req,res) => {
+    const sqlSelect = "SELECT * FROM books;";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+})
+
+app.post("/api/book/add", (req, res) => {
+    const bookName = req.body.bookName;
+    const bookAuthor = req.body.bookAuthor;
+    const bookCategory = req.body.bookCategory;
+    const bookDescription = req.body.bookDescription;
+    const bookTime = req.body.bookTime;
+    const bookOpr = req.body.bookOpr;
+
+    const sqlInsert = "INSERT INTO books (bookName, bookAuthor, bookDescription, bookCategory, bookTime, bookOpretor) VALUES (?,?,?,?,?,?)";
+    db.query(sqlInsert, [bookName, bookAuthor, bookDescription, bookCategory, bookTime, bookOpr], (err, result) => {
+        console.log(result);
+    })
+})
+
+app.post("/api/book/update", (req, res) => {
+    const bookName = req.body.bookName;
+    const bookAuthor = req.body.bookAuthor;
+    const bookCategory = req.body.bookCategory;
+    const bookDescription = req.body.bookDescription;
+    const bookTime = req.body.bookTime;
+    const bookOpr = req.body.bookOpr;
+
+    const sqlUpdate = "UPDATE books SET bookAuthor = ?, bookCategory = ?, bookDescription = ?, bookTime = ?, bookOpretor = ? WHERE bookName = ?;";
+    db.query(sqlUpdate, [bookAuthor, bookCategory, bookDescription, bookTime, bookOpr, bookName], (err, result) => {
+        console.log(err);
+    })
+})
+
+app.post("/api/book/delete", (req, res) => {
+    const bookName = req.body.bookName;
+
+    const sqlDelete = "DELETE FROM books WHERE bookName = ?";
+    db.query(sqlDelete, [bookName], (err, result) => {
+        console.log(result);
+    })
+})
+
+app.listen(3022, () => {
+    console.log("running on port 3022");
+})
